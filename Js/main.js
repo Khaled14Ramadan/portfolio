dc354df15ffe7b5a10f45191f69d4d3db34e9066
khaled Ramadan <khaled14ramadan@gmail.com>
2022-06-25 16:41:33 +0200

    upload the design

---
+++ Js/main.js
@@ -0,0 +1,35 @@
+let menu_khaled = document.getElementById("khaled");
+
+// to show the nav bar in mobile
+let menu = function () {
+    menu_khaled.classList.toggle("toggle");
+    console.log(menu_khaled);
+}
+
+//to show background of nav after header (jQuery)
+
+let aboutOffset = $("#about").offset().top;
+displayNav($(window).scrollTop());//if reload when secroll after about section
+
+$(window).scroll(function () {
+
+    let valueScroll = $(window).scrollTop();
+    displayNav(valueScroll);
+});
+
+function displayNav(v){
+    if(v > aboutOffset - 50)
+    {
+        $("#btnUp").fadeIn(500);
+    }
+    else{
+        $("#btnUp").fadeOut(500);
+    }
+}
+
+///this write behavior scrool by jquery
+
+$("a[href ^='#']").click(function(){
+    let aHref = $(this.href).offset().top;
+    $("html , body").animate({scrollTop:aHref} , 2000);
+});
