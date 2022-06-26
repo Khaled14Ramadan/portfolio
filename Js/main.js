0ea3fbd4c18878aeb4ff7dd3d6956f3c5ded46d2
khaled Ramadan <khaled14ramadan@gmail.com>
2022-06-26 05:45:41 +0200

    new

---
+++ Js/main.js
@@ -0,0 +1,7 @@
+let menu_khaled = document.getElementById("khaled");
+
+// to show the nav bar in mobile
+let menu = function () {
+    menu_khaled.classList.toggle("toggle");
+    console.log(menu_khaled);
+}
